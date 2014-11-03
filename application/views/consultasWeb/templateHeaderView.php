a35a2548d3d9b3a90386d1f116c3c01b86b48382
cristianchaparroa <cristianchaparroa@gmail.com>
2014-11-02 21:54:32 -0500

    Modificacion menu template

--- application/views/consultasWeb/templateHeaderView.php
+++ application/views/consultasWeb/templateHeaderView.php
@@ -6,10 +6,10 @@
 		<link href="<?php echo base_url('assets/css/menu.css');?>" rel="stylesheet" type="text/css">
 		<link href="//cdn.datatables.net/plug-ins/a5734b29083/integration/jqueryui/dataTables.jqueryui.css" rel="stylesheet" type="text/css">
 
-		<script asyn type="text/javascript" src="<?php echo base_url('assets/js/jquery-2.1.1.min.js');?>"></script>
-		<script asyn type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
-		<script asyn type="text/javascript" src="<?php echo base_url('assets/js/fecha.js');?>"></script>
-		<script asyn type="text/javascript" src="<?php echo base_url('assets/js/setup.datatables.js');?>"></script>
+		<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-2.1.1.min.js');?>"></script>
+		<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
+		<script type="text/javascript" src="<?php echo base_url('assets/js/fecha.js');?>"></script>
+		<script type="text/javascript" src="<?php echo base_url('assets/js/setup.datatables.js');?>"></script>
 
 		<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10-dev/js/jquery.dataTables.min.js"></script>
 		<script asyn type="text/javascript" src="//cdn.datatables.net/plug-ins/a5734b29083/integration/jqueryui/dataTables.jqueryui.js"></script>
