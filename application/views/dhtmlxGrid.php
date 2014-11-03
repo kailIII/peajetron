11a9291a8b6674a8c09e385b337ddf34bda5ec03
José Daniel Peña <daniel@AracniaStudios.aracniastudios.net>
2014-11-03 13:30:55 -0500

    Cambio de dhtmlx a la version pro, Agrego libreria phpmailer, Ultima version Base de datos, Avances aplicacion peajetron

--- application/views/dhtmlxGrid.php
+++
@@ -1,9 +0,0 @@
-			<div id="gridbox"></div>
-			<script type="text/javascript">
-				mygrid = new dhtmlXGridObject('gridbox');
-				mygrid.setImagePath('../third_party/dhtmlx/codebase/imgs/');
-				mygrid.setHeader('<?php echo $header?>');
-				mygrid.setSkin("dhx_terrace");
-				mygrid.init();
-				mygrid.load("./data");
-			</script>
