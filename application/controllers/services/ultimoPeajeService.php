ad1e48417c1e39ff7d514f7dcef6b25a74343b05
root <root@d1stkfactory>
2014-11-05 07:05:58 +0000

    Web service  ultimoPeajeService : modulo 5

---
+++ application/controllers/services/ultimoPeajeService.php
@@ -0,0 +1,54 @@
+<?php
+
+class UltimoPeajeService extends  CI_Controller{
+    public function __construct()
+    {
+      parent::__construct();
+      $this->load->model('usuarios');
+      $this->load->model('vehiculos');
+      $this->load->model('cobros');
+    }
+    public function index()
+    {
+
+        if( isset($_REQUEST['id']) &&  $_REQUEST['id']!=''  )
+        {
+            $idUser  = $_REQUEST['id'];
+            $result =  $this->vehiculos->vehiculosPropietario( $idUser );
+            if( $result!=FALSE)
+            {
+               $placas = array();
+               foreach( $result as $vehiculo )
+               {
+                  $id = $vehiculo->id_vehiculo;
+                  $placa = $vehiculo->placa;
+                  $cobro = $this->cobros->ultimoPeajeCruzado( $id, $idUser );
+
+                  if( $cobro != FALSE )
+                  {
+                     $placa = array(
+                       'placa'     => $placa,
+                       'valor'     => $cobro->valor,
+                       'matricula' => $placa,
+                       'ruta'      => $cobro->hora,
+                       'fecha'     => $cobro->fecha,
+                       'ubicacion' => 'nose'
+                     );
+                     $placas [] = $placa;
+                    
+                  }//fin if cobro false
+               }//endforeach
+               $json  = array(
+                   'placas' =>  $placas,
+                   'status' => TRUE,
+               );
+               echo json_encode($json );
+            }//endif
+        }
+        else{
+            $json =  array( 'status'=>FALSE );
+            echo json_encode( $json );
+        }
+
+    }
+}
